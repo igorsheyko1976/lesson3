8cb7c282595291428dbde0a94ac28c4d5dd64855
igorsheyko1976 <sheyko-iv@mail.ru>
2024-03-22 20:57:47 +0300

    add all const

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -7,7 +8,16 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра ТИР")
-icon = pygame.load.image("")
+icon = pygame.image.load("image/1.jpg")
+pygame.display.set_icon(icon)
+target_img = pygame.image.load("image/target.png")
+target_width = 80
+target_height = 80
+
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+target_image = pygame.image.load(“img / target.png”)
 
 running = True
 while running:
